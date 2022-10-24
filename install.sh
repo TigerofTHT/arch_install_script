733b9d0423a001ed7b4a9274e55c2f777b30c63a
TigerofTHT <39875308+TigerofTHT@users.noreply.github.com>
2022-10-24 22:29:05 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -27,20 +27,37 @@ timedatectl status
 while getopts "m" opt
 do
         case $opt in 
-            m)
-                    echo -e "\nPreparing disk with MBR...\n"
-                    parted -s -a optimal $disk mklabel msdos \
-                                        mkpart primary linux-swap 0% 128M \
+                m)
+                        echo -e "\nPreparing disk with MBR...\n"
+                        parted -s -a optimal $disk mklabel msdos \
+                                        mkpart primary linux-swap 0% 4G \
                                         mkpart primary ext4 4G 100% print
-                    mkswap $disk"1"
-                    mkfs.ext4 $disk"2"
-                    swapon $disk"1"
-                    mount $disk"2" /mnt
-                    pacstrap -K /mnt base linux linux-firmware nano dhcpcd syslinux 
-                    genfstab -U /mnt >> /mnt/etc/fstab
-                    export -f chrootenv
-                    arch-chroot /mnt /bin/bash -c chrootenv
-                    ;;
+                        mkswap $disk"1"
+                        mkfs.ext4 $disk"2"
+                        swapon $disk"1"
+                        mount $disk"2" /mnt
+                        pacstrap -K /mnt base linux linux-firmware nano dhcpcd syslinux 
+                        genfstab -U /mnt >> /mnt/etc/fstab
+                        export -f chrootenv
+                        arch-chroot /mnt /bin/bash -c chrootenv
+                        ;;
+                
+                u)      echo -e "\nPreparing disk with UEFI...\n"
+                        parted -s -a optimal $disk mklabel msdos \
+                                        mkpart primary fat32 0% 256M \
+                                        mkpart primary linux-swap 256M 4G \
+                                        mkpart primary ext4 4G 100% print
+                        mkfs.fat -F 32 $disk"1"
+                        mkswap $disk"2"
+                        mkfs.ext4 $disk"3"
+                        swapon $disk"2"
+                        mount $disk"3" /mnt
+                        mount --mkdir $disk"1" /mnt/boot
+                        pacstrap -K /mnt base linux linux-firmware nano dhcpcd syslinux 
+                        genfstab -U /mnt >> /mnt/etc/fstab
+                        export -f chrootenv
+                        arch-chroot /mnt /bin/bash -c chrootenv
+                        ;;
         esac 
 done
 
